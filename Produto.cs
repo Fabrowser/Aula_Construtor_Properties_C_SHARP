8e0eb62b13ce0eef976fcc84b69ebfc15ac8c34f
Fabrowser <79614829+Fabrowser@users.noreply.github.com>
2022-07-14 00:04:08 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioClasseProduto
+{
+    class Produto
+    {
+        //Atributos
+
+        private string _nome;
+        private double _preco;
+        public int Quantidade { get; private set; } // Auto Properties
+
+        public Produto()// Construtor
+        {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade) // Construtor
+        {
+            _nome = nome;
+            _preco = preco;
+            Quantidade = quantidade;
+
+        }
+
+        public string Nome // Properties
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+
+                ;
+            }
+        }
+
+        public double Preco // Properties
+        {
+            get { return _preco; }
+
+        }
+
+        // Métodos publicos
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * _preco;
+        }
+
+        public void AdicionaProduto(int q)
+        {
+            Quantidade = Quantidade + q;
+        }
+
+        public void RemoverProdutos(int q)
+        {
+
+            Quantidade = Quantidade - q;
+        }
+
+        public override string ToString()
+        {
+            return ($"Dados atualizados:\nNome: {Nome} \nPreço: {_preco} \nQuantidade:{Quantidade} \n" +
+                $"Total: {ValorTotalEmEstoque().ToString("F2")}\n");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ExercicioClasseProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*Console.WriteLine("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o preço do produto: ");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade em estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());*/
+
+
+            Produto p1 = new Produto("TV 4K", 900.00,10);
+
+            p1.Nome = "T";
+            
+            Console.WriteLine(p1);
+
+            Console.WriteLine("Set / Properties: " + p1.Nome);
+            Console.WriteLine("Get / Properties: " + p1.Preco);
+            Console.WriteLine("Get / Auto-Properties: " + p1.Quantidade);
+
+
+
+            /*Produto p1 = new Produto(nome, preco, quantidade);
+
+
+           
+
+            //Adiciona no estoque
+            Console.WriteLine("Atualizar estoque: \n");
+            Console.WriteLine("Digite a quantidade adicional: ");
+            int adicional = int.Parse(Console.ReadLine());
+
+            p1.AdicionaProduto(adicional);
+
+            Console.WriteLine(p1);
+
+
+            //Remove do estoque
+            Console.WriteLine("Atualizar estoque: \n");
+            Console.WriteLine("Digite a quantidade removida: ");
+            int removido = int.Parse(Console.ReadLine());
+
+            p1.RemoverProdutos(removido);
+
+
+            Console.WriteLine(p1);*/
+
+
+
+        }
+    }
+}
